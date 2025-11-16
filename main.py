fa8d0fde1e0a0b7ef13b7979967c18df38786440
Vlad <artasteasusprogram@gmail.com>
2025-11-16 23:22:55 +0300

    add_2_function

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from src.masks import get_mask_account, get_mask_card_number
-from src.widget import mask_account_card
+from src.widget import mask_account_card, get_date
 
 if __name__ == "__main__":
     card = input("Введите номер карты: ")
     account = input("Введиет номер счёта: ")
     user_input = input("Введите тип и номер (например, Visa 7000...): ")
+    date_string = input("Введите дату: ")
     print(get_mask_card_number(card))
     print(get_mask_account(account))
     print(mask_account_card(user_input))
+    print(get_date(date_string))
--- src/widget.py
+++ src/widget.py
@@ -10,3 +10,15 @@ def mask_account_card(user_input: str) -> str:
         return f"Счет {get_mask_account(number)}"
     else:
         return f"{name} {get_mask_card_number(number)}"
+
+
+def get_date(date_string: str) -> str:
+    """
+    Принимает: '2024-03-11T02:26:18.671407'
+    Возвращает: '11.03.2024'
+    """
+    date_part = date_string.split("T")[0]
+
+    year, month, day = date_part.split("-")
+
+    return f"{day}.{month}.{year}"
