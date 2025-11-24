f6c52c8a440be48b029bfaed4e5ceb81d3945341
Kositsyn Vladislav <artasteasusprogram@gmail.com>
2025-11-24 18:54:37 +0300

    add_sort_by_date_in_processing.py

--- src/processing.py
+++ src/processing.py
@@ -6,3 +6,7 @@ def filter_by_state(dict_list: list, state: str = "EXECUTED") -> list:
         else:
             continue
     return new_list
+
+
+def sort_by_date(dict_list: list, reverse: bool = True) -> list:
+    return sorted(dict_list, key=lambda x: x['date'], reverse=reverse)
