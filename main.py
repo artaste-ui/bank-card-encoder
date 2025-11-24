3b96249fb04678a522304a188d457af3d4e0349d
Kositsyn Vladislav <artasteasusprogram@gmail.com>
2025-11-24 20:24:47 +0300

    main_10_1

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ if __name__ == "__main__":
     card = input("Введите номер карты: ")
     account = input("Введиет номер счёта: ")
     user_input = input("Введите тип и номер (например, Visa 7000...): ")
-    date_string = input("Введите дату: ")
     print(get_mask_card_number(card))
     print(get_mask_account(account))
     print(mask_account_card(user_input))
-    print(get_date(date_string))
