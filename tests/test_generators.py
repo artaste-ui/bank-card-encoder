514359041c94740da01af7a308ea758968b54049
Kositsyn Vladislav <artasteasusprogram@gmail.com>
2025-12-09 19:06:51 +0300

    add tests for generators

---
+++ tests/test_generators.py
@@ -0,0 +1,130 @@
+import pytest
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+
+
+# Полный список транзакций из твоего проекта (все, что есть)
+transactions = [
+    {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {
+            "amount": "9824.07",
+            "currency": {"name": "USD", "code": "USD"}
+        },
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702"
+    },
+    {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {
+            "amount": "79114.93",
+            "currency": {"name": "RUB", "code": "RUB"}
+        },
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708664524232713125",
+        "to": "Счет 75651667383060284188"
+    },
+    {
+        "id": 873106923,
+        "state": "EXECUTED",
+        "date": "2019-03-23T01:09:46.296404",
+        "operationAmount": {
+            "amount": "43318.34",
+            "currency": {"name": "руб.", "code": "RUB"}
+        },
+        "description": "Перевод со счета на счет",
+        "from": "Счет 44812258784861134719",
+        "to": "Счет 7448963641752119160"
+    },
+    {
+        "id": 594226727,
+        "state": "CANCELED",
+        "date": "2018-09-12T21:27:25.241689",
+        "operationAmount": {
+            "amount": "67314.70",
+            "currency": {"name": "руб.", "code": "RUB"}
+        },
+        "description": "Перевод организации",
+        "from": "Visa Platinum 1246377376343588",
+        "to": "Счет 14211924144426031657"
+    },
+    {
+        "id": 895315941,
+        "state": "EXECUTED",
+        "date": "2018-08-19T04:27:37.904916",
+        "operationAmount": {
+            "amount": "56883.54",
+            "currency": {"name": "USD", "code": "USD"}
+        },
+        "description": "Перевод с карты на карту",
+        "from": "Visa Classic 6831982476737658",
+        "to": "Visa Platinum 8990922113665229"
+    }
+]
+
+
+def test_filter_by_currency():
+    # USD транзакции
+    usd_gen = filter_by_currency(transactions, "USD")
+    usd_list = list(usd_gen)
+    assert len(usd_list) == 2
+    assert usd_list[0]["id"] == 939719570
+    assert usd_list[1]["id"] == 895315941
+
+    # RUB транзакции
+    rub_gen = filter_by_currency(transactions, "RUB")
+    rub_list = list(rub_gen)
+    assert len(rub_list) == 3
+
+    # Нет такой валюты
+    empty = list(filter_by_currency(transactions, "EUR"))
+    assert empty == []
+
+    # Пустой список
+    assert list(filter_by_currency([], "USD")) == []
+
+
+def test_transaction_descriptions():
+    desc_gen = transaction_descriptions(transactions)
+    descriptions = list(desc_gen)
+
+    expected = [
+        "Перевод организации",
+        "Перевод со счета на счет",
+        "Перевод со счета на счет",
+        "Перевод организации",
+        "Перевод с карты на карту"
+    ]
+    assert descriptions == expected
+
+    # Пустой список
+    assert list(transaction_descriptions([])) == []
+
+
+@pytest.mark.parametrize("start, stop, expected", [
+    (1, 3, [
+        "0000 0000 0000 0001",
+        "0000 0000 0000 0002",
+        "0000 0000 0000 0003"
+    ]),
+    (9999999999999997, 9999999999999999, [
+        "9999 9999 9999 9997",
+        "9999 9999 9999 9998",
+        "9999 9999 9999 9999"
+    ]),
+    (100, 102, [
+        "0000 0000 0000 0100",
+        "0000 0000 0000 0101",
+        "0000 0000 0000 0102"
+    ])
+])
+def test_card_number_generator(start, stop, expected):
+    result = list(card_number_generator(start, stop))
+    assert result == expected
+
+    # Проверка, что генератор можно использовать несколько раз
+    assert list(card_number_generator(start, stop)) == expected
