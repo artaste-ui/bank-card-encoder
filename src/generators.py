dbb06bcdb8c0b57b2d472dbf44b37b4eb28307fd
Kositsyn Vladislav <artasteasusprogram@gmail.com>
2025-12-09 17:03:04 +0300

    add func card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -8,3 +8,10 @@ def filter_by_currency(transactions, currency):
 def transaction_descriptions(transactions):
     '''Возвращает описание каждой операции по очереди'''
     return (transaction["description"] for transaction in transactions)
+
+
+def card_number_generator(start, stop):
+    '''Возвращает сгенерированный номер карты'''
+    for n in range(start, stop + 1):
+        card = f"{n:016}"
+        yield f"{card[:4]} {card[4:8]} {card[8:12]} {card[12:]}"
