b6981903e802902075186ea8977c07b62ddc752b
Kositsyn Vladislav <artasteasusprogram@gmail.com>
2025-12-11 11:14:20 +0300

    processing by black

--- src/generators.py
+++ src/generators.py
@@ -1,18 +1,18 @@
 def filter_by_currency(transactions, currency):
-    '''Возвращает поочерёдно словарь, отфильтрованный по заданному параметру currency'''
+    """Возвращает поочерёдно словарь, отфильтрованный по заданному параметру currency"""
     for x in transactions:
         if x.get("operationAmount", {}).get("currency", {}).get("code") == currency:
             yield x
 
 
 def transaction_descriptions(transactions):
-    '''Возвращает описание каждой операции по очереди.'''
+    """Возвращает описание каждой операции по очереди."""
     for transaction in transactions:
         yield transaction["description"]
 
 
 def card_number_generator(start, stop):
-    '''Возвращает сгенерированный номер карты'''
+    """Возвращает сгенерированный номер карты"""
     for n in range(start, stop + 1):
         card = f"{n:016}"
         yield f"{card[:4]} {card[4:8]} {card[8:12]} {card[12:]}"
